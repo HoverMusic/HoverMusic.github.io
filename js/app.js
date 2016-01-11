b48952afdb6532d7e1eb472f6113812735534f2e
Pat <patharryux@gmail.com>
2016-01-11 15:05:45 -0800

    Add comment about browser

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@ $( document ).ready( function(){
   app.current_scene = {}
   app.current_scene.group = '';
 
-  app.synth_01 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
-  app.synth_02 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
-  app.synth_03 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
-  app.synth_04 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
+  app.audio = {};
+
+  app.audio.synth_01 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
+  app.audio.synth_02 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
+  app.audio.synth_03 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
+  app.audio.synth_04 = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
 
   app.audio = new App.Audio({
-    hello: 'world',
-    synth_01: app.synth_01,
-    synth_02: app.synth_02,
-    synth_03: app.synth_03,
-    synth_04: app.synth_04    
+    synth_01: app.audio.synth_01,
+    synth_02: app.audio.synth_02,
+    synth_03: app.audio.synth_03,
+    synth_04: app.audio.synth_04
   });
 
 });
